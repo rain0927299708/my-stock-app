9c66dcf51f60d0ea76fa25b4487955cfa2609372
rain0927299708 <125946358+rain0927299708@users.noreply.github.com>
2026-05-22 13:38:44 +0800

    Change app view to WEB_BROWSER in main.py

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ def main(page: ft.Page):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
-    ft.app(target=main, host="0.0.0.0", port=port)
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=port)
